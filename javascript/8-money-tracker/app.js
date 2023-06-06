b7676e0886f862cb667a77e4a9a183c6ac34b11e
jaganjavid <jagantutor1@gmail.com>
2023-06-06 20:48:11 +0530

    money tracker add item done

--- javascript/8-money-tracker/app.js
+++ javascript/8-money-tracker/app.js
@@ -11,8 +11,14 @@
 
 const ItemCtrl = (function(){
 
-    //  DATA STRUCTURE / STATE
 
+    const Item = function(id, name, money){
+        this.id = id;
+        this.name = name;
+        this.money = money
+    }
+
+    //  DATA STRUCTURE / STATE
     const data = {
         items: [
             {id:0, name:"clothes", money: 200},
@@ -26,6 +32,41 @@ const ItemCtrl = (function(){
     return {
         getItems: function(){
             return data.items;
+        },
+        addItem: function(name, money){
+            let ID;
+
+            // CREATE A ID
+            if(data.items.length > 0){
+                ID = data.items[data.items.length - 1].id + 1
+            } else{
+                ID = 0;
+            }
+        
+            // CONVERT STRING INTO MONEY
+            money = parseInt(money);
+
+            // CREATE A NEW ITEM 
+            const newItem = new Item(ID, name, money);
+
+           // ADD TO ITEM ARRAY
+           data.items.push(newItem);
+            
+           return newItem;
+        },
+        getTotalMoney: function(){
+            let total = 0;
+
+            if(data.items.length > 0){
+                data.items.forEach(function(item){
+                    total += item.money
+                    data.totalMoney = total;
+                })
+            } else{
+                return data.totalMoney = 0;
+            }
+
+            return total;
         }
     }
 
@@ -58,11 +99,44 @@ const UICtrl = (function(){
         document.querySelector(".delete-btn").style.display = "none";
         document.querySelector(".back-btn").style.display = "none";
     },
+    showEditState: function(){
+        document.querySelector(".add-btn").style.display = "none";
+        document.querySelector(".update-btn").style.display = "inline";
+        document.querySelector(".delete-btn").style.display = "inline";
+        document.querySelector(".back-btn").style.display = "inline";
+    },
     getItemInput: function(){
         return {
             name: document.querySelector("#item-name").value,
             money: document.querySelector("#item-money").value
         }
+    },
+    addListItem: function(item){
+     // CREATE A LI ELEMENT
+     const li = document.createElement("li");
+
+    //  ADD CLASS TO LI
+    li.className = "collection-item";
+
+    // ADD ID
+    li.id = `item-${item.id}`;
+
+    // INSERT HTML
+    li.innerHTML = `<strong>${item.name} : <em>${item.money}$</em></strong>
+    <a href="#" class="secondary-content edit-item">
+        <i class="fa fa-pencil"></i>
+    </a>`;
+
+    // INSERT ITEM TO UL
+    document.querySelector(".collection").appendChild(li);
+
+    },
+    showTotalMoney: function(totalMoney){
+        document.querySelector(".total-money").textContent = totalMoney;
+    },
+    clearInputState: function(){
+        document.querySelector("#item-name").value = "";
+        document.querySelector("#item-money").value = "";
     }
 
   }
@@ -75,6 +149,9 @@ const App = (function(ItemCtrl, UICtrl){
 
         // ADD ITEM EVENT
         document.querySelector(".add-btn").addEventListener("click", itemAddSubmit);
+
+        // ITEM TO EDIT
+        document.querySelector(".collection").addEventListener("click", itemToEdit);
     }
 
 
@@ -87,11 +164,37 @@ const App = (function(ItemCtrl, UICtrl){
        // Validate
 
        if(input.name === "" || input.money === ""){
-        alert("Please fill the fileds")
+        alert("Please fill the fileds");
        }else {
-        console.log(input);
+        //  Add Item   
+        const newItem = ItemCtrl.addItem(input.name, input.money);
+
+        // ADD ITEM TO UI LIST
+        UICtrl.addListItem(newItem);
+
+        // GET TOTAL MONEY 
+        const totalMoney = ItemCtrl.getTotalMoney(); 
+        
+        // ADD TOTAL MONEY TO UI
+        UICtrl.showTotalMoney(totalMoney);
+
+        // CLEAR A UI INPUT
+        UICtrl.clearInputState();
+        
        }
 
+    }
+
+
+    const itemToEdit = function(e){
+        e.preventDefault();
+
+        if(e.target.parentElement.classList.contains("edit-item")){
+            
+            // SHOW ALL BUTTON
+            UICtrl.showEditState();
+        }
+    
     }
 
 
@@ -104,6 +207,13 @@ const App = (function(ItemCtrl, UICtrl){
             
             if(items.length > 0){
                 UICtrl.populateItemsList(items);
+                
+                // GET TOTAL MONEY 
+                const totalMoney = ItemCtrl.getTotalMoney(); 
+                
+                // ADD TOTAL MONEY TO UI
+                UICtrl.showTotalMoney(totalMoney);
+
                 document.querySelector(".no-item").style.display = "none";
             } else {
                 document.querySelector(".no-item").style.display = "block";
